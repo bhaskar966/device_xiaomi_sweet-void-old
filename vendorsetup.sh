7423b8246238fb7d16a928fb0c87e754103b5c74
bhaskar966 <86063625+bhaskar966@users.noreply.github.com>
2023-04-14 10:56:04 +0530

    Chnage Kernel path to kernel/xiaomi/sweet
    from kernel/xiaomi/sm6150
    
    Signed-off-by: bhaskar966 <deybhaskar999@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,4 +20,4 @@ git clone https://github.com/PixelExperience/packages_resources_devicesettings p
 
 # Kernel
 git clone -b 17 --depth=1 https://gitlab.com/PixelOS-Devices/playgroundtc.git prebuilts/clang/host/linux-x86/clang-playground
-git clone -b courbet-13 --depth=1 https://github.com/vantoman/kernel_xiaomi_sm6150.git kernel/xiaomi/sm6150
+git clone -b courbet-13 --depth=1 https://github.com/vantoman/kernel_xiaomi_sm6150.git kernel/xiaomi/sweet
