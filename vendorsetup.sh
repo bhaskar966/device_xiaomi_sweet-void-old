5b8b447f9b35892f67a9aea8996d95dff782b140
bhaskar966 <86063625+bhaskar966@users.noreply.github.com>
2023-04-16 08:55:59 +0530

    Change MIUI Cam branch to T from topaz
    Signed-off-by: bhaskar966 <deybhaskar999@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -10,7 +10,7 @@ git clone https://github.com/ArrowOS/android_hardware_qcom_media -b arrow-13.0-c
 git clone https://github.com/ArrowOS/android_hardware_qcom_display -b arrow-13.0-caf-sm8150 hardware/qcom-caf/sm8150/display
 
 # MIUI Camera
-git clone https://gitlab.com/mrfox2003/sweet-miuicamera.git -b topaz vendor/xiaomi/sweet-miuicamera
+git clone https://gitlab.com/mrfox2003/sweet-miuicamera.git -b T vendor/xiaomi/sweet-miuicamera
 
 # Vendor
 git clone https://github.com/bhaskar966/vendor_xiaomi_sweet.git -b 13.0 vendor/xiaomi/sweet
