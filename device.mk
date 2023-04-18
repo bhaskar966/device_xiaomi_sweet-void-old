7bd8127d53a1527a60aa6d71844c65a47f42d146
bhaskar966 <deybhaskar999@gmail.com>
2023-04-18 18:43:42 +0530

    sweet: overlay: Configure aux camera for Aperture
    Signed-off-by: bhaskar966 <deybhaskar999@gmail.com>

--- device.mk
+++ device.mk
@@ -402,6 +402,7 @@ PRODUCT_PACKAGES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage \
     $(LOCAL_PATH)/overlay-void
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
